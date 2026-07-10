d7c8de4a3cb64f3a4496ca00851d8bf974750eea
ElliAlv <vinlandyl@gmail.com>
2026-07-10 02:46:48 -0700

    improve code of main menu UI

--- components/group-menu/group-menu.tsx
+++ components/group-menu/group-menu.tsx
@@ -7,7 +7,6 @@ import { selectGroup } from "../../lib/features/groupSlice";
 import { getGroups } from "../../lib/get-groups";
 import Button from "./top-menu-button";
 
-
 export default function GroupMenu() {
   //Redux hooks used for main menu interaction
   const selectedGroup = useAppSelector(
@@ -20,13 +19,14 @@ export default function GroupMenu() {
   const [buttonClasses, setButtonClasses] = useState<string[]>([]);
   const cssClassesArray: string[] = [];
 
-
   //Gives red text to the selected group (menu option) and white text to the others
   function setDefaultColours() {
     groups.map((item) =>
       item === selectedGroup
-        ? cssClassesArray.push("text-red-500 bg-[#000000]")
-        : cssClassesArray.push("text-white bg-[#000000]"),
+        ? cssClassesArray.push("text-red-500 bg-[#000000] hover:bg-[#1A1A1A]")
+        : cssClassesArray.push(
+            "text-white bg-[#000000] hover:text-red-500 hover:bg-[#1A1A1A]",
+          ),
     );
     setButtonClasses(cssClassesArray);
   }
@@ -42,23 +42,6 @@ export default function GroupMenu() {
     setDefaultColours();
   }
 
-  //Gives red text and gray background to the button being hover over
-  function handleButtonMouseOver(button: string) {
-    groups.map((item) =>
-      item === button
-        ? cssClassesArray.push("text-red-500 bg-[#1A1A1A]")
-        : item === selectedGroup
-          ? cssClassesArray.push("text-red-500 bg-[#000000]")
-          : cssClassesArray.push("text-white bg-[#000000]"),
-    );
-    setButtonClasses(cssClassesArray);
-  }
-
-  //Returns a black background to the button on mouse out. Gives red text to the selected group and white text to the others
-  function handleButtonMouseOut() {
-    setDefaultColours();
-  }
-
   return (
     <div className="flex justify-center h-16 bg-[#000000]">
       <menu className="flex flex-row items-center">
@@ -66,8 +49,6 @@ export default function GroupMenu() {
           <li key={menuItem}>
             <Button
               onButtonClick={handleMenuClick}
-              onButtonMouseOver={handleButtonMouseOver}
-              onButtonMouseOut={handleButtonMouseOut}
               cssClasses={buttonClasses[index]}
             >
               {menuItem}
--- components/group-menu/top-menu-button.tsx
+++ components/group-menu/top-menu-button.tsx
@@ -4,39 +4,21 @@ import { ReactNode } from "react";
 
 interface ButtonProps {
   onButtonClick: (group: string) => void;
-  onButtonMouseOver: (button: string) => void;
-  onButtonMouseOut: () => void;
   cssClasses: string;
   children: ReactNode;
 }
 
 export default function Button({
   onButtonClick,
-  onButtonMouseOver,
-  onButtonMouseOut,
   cssClasses,
   children,
 }: ButtonProps) {
-  
-  function handleMouseOver(e: React.MouseEvent<HTMLButtonElement>) {
-    onButtonMouseOver(e.currentTarget.textContent);
-  }
-
-  function handleMouseOut() {
-    onButtonMouseOut();
-  }
-
   function handleClick(e: React.MouseEvent<HTMLButtonElement>) {
     onButtonClick(e.currentTarget.textContent);
   }
 
   return (
-    <button
-      onMouseOver={handleMouseOver}
-      onMouseOut={handleMouseOut}
-      onClick={handleClick}
-      className={cssClasses + " py-2 px-5 m-0.5"}
-    >
+    <button onClick={handleClick} className={cssClasses + " py-2 px-5 m-0.5"}>
       {children}
     </button>
   );
