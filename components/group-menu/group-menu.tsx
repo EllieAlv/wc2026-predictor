ee4936c2e276a88d2605469475fa392ff702e08b
ElliAlv <vinlandyl@gmail.com>
2026-07-10 02:15:46 -0700

    improve code of main menu UI

--- components/group-menu/group-menu.tsx
+++ components/group-menu/group-menu.tsx
@@ -7,6 +7,7 @@ import { selectGroup } from "../../lib/features/groupSlice";
 import { getGroups } from "../../lib/get-groups";
 import Button from "./top-menu-button";
 
+
 export default function GroupMenu() {
   //Redux hooks used for main menu interaction
   const selectedGroup = useAppSelector(
@@ -19,25 +20,26 @@ export default function GroupMenu() {
   const [buttonClasses, setButtonClasses] = useState<string[]>([]);
   const cssClassesArray: string[] = [];
 
-  //useEffect hook used for correctly loading the UI colors of the main menu buttons while preventing infinite re-renders
-  useEffect(() => {
+
+  //Gives red text to the selected group (menu option) and white text to the others
+  function setDefaultColours() {
     groups.map((item) =>
       item === selectedGroup
         ? cssClassesArray.push("text-red-500 bg-[#000000]")
         : cssClassesArray.push("text-white bg-[#000000]"),
     );
     setButtonClasses(cssClassesArray);
+  }
+
+  //useEffect hook used for correctly loading the UI colors of the main menu buttons while preventing infinite re-renders
+  useEffect(() => {
+    setDefaultColours();
   }, [selectedGroup]);
 
   //Gives red text to the selected button and white text to the others
   function handleMenuClick(group: string) {
     dispatch(selectGroup(group));
-    groups.map((item) =>
-      item === selectedGroup
-        ? cssClassesArray.push("text-red-500 bg-[#000000]")
-        : cssClassesArray.push("text-white bg-[#000000]"),
-    );
-    setButtonClasses(cssClassesArray);
+    setDefaultColours();
   }
 
   //Gives red text and gray background to the button being hover over
@@ -45,19 +47,16 @@ export default function GroupMenu() {
     groups.map((item) =>
       item === button
         ? cssClassesArray.push("text-red-500 bg-[#1A1A1A]")
-        : item === selectedGroup ? cssClassesArray.push("text-red-500 bg-[#000000]") : cssClassesArray.push("text-white bg-[#000000]"),
+        : item === selectedGroup
+          ? cssClassesArray.push("text-red-500 bg-[#000000]")
+          : cssClassesArray.push("text-white bg-[#000000]"),
     );
     setButtonClasses(cssClassesArray);
   }
 
   //Returns a black background to the button on mouse out. Gives red text to the selected group and white text to the others
   function handleButtonMouseOut() {
-    groups.map((item) =>
-      item === selectedGroup
-        ? cssClassesArray.push("text-red-500 bg-[#000000]")
-        : cssClassesArray.push("text-white bg-[#000000]"),
-    );
-    setButtonClasses(cssClassesArray);
+    setDefaultColours();
   }
 
   return (
